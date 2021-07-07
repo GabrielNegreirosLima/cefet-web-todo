8947750820105474ce707b9028a626755d629556
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2021-07-07 20:56:56 -0300

    Add Exercicio 0

---
+++ tarefas.js
@@ -0,0 +1,22 @@
+let tarefas = [
+    {
+        nome: "Fazer essa prática de Web",
+        categoria: "estudos",
+        realizada: true
+    },
+    {
+        nome: "Terminar o frontend do trabalho de Web",
+        categoria: "estudos",
+        realizada: false
+    },
+    {
+        nome: "Ir no sacolão",
+        categoria: "compras",
+        realizada: true
+    },
+    {
+        nome: "Jogar um CTF",
+        categoria: "lazer",
+        realizada: false
+    }
+]
