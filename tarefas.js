3dc730653dbd63fd94e4df94016087821356f6a6
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2021-07-08 13:19:28 -0300

    Add Exercicio 1 e 2

--- tarefas.js
+++ tarefas.js
@@ -1,3 +1,4 @@
+// Exercício 0 
 let tarefas = [
     {
         nome: "Fazer essa prática de Web",
@@ -19,4 +20,43 @@ let tarefas = [
         categoria: "lazer",
         realizada: false
     }
-]
+];
+
+
+// Exercício 1
+const listaTarefasEl = document.querySelector("#lista-tarefas");
+
+function insereTarefaNaPagina(tarefa){
+
+    const itemTarefaEl = document.createElement("li");
+    itemTarefaEl.classList.add("item-tarefa");
+
+    if(tarefa.realizada){
+        itemTarefaEl.classList.add("marcado");
+    }
+
+    itemTarefaEl.classList.add(`categoria-${tarefa.categoria}`);
+
+    itemTarefaEl.innerHTML = tarefa.nome;
+
+    listaTarefasEl.appendChild(itemTarefaEl);
+}
+
+listaTarefasEl.innerHTML = "";
+tarefas.forEach(insereTarefaNaPagina);
+
+
+// Exercício 2
+const botaoNovaTarefaEl = document.querySelector("#incluir-nova-tarefa");
+botaoNovaTarefaEl.addEventListener("click", e => {
+    const nomeEL = document.querySelector("#nova-tarefa-nome");
+    const categoriaEl = document.querySelector("#nova-tarefa-categoria");
+    
+    const tarefa = {
+        nome: nomeEL.value,
+        categoria: categoriaEl.value,
+        realizada: false
+    };
+    tarefas.push(tarefa);
+    insereTarefaNaPagina(tarefa);
+});
